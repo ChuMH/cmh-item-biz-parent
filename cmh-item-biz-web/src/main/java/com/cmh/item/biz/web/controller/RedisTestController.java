e04b8bac569c4cbd24a141a87e51365a0dbd8a63
haha <1034357113@qq.com>
2020-08-02 10:41:10 +0800

    redis测试

--- cmh-item-biz-web/src/main/java/com/cmh/item/biz/web/controller/RedisTestController.java
+++ cmh-item-biz-web/src/main/java/com/cmh/item/biz/web/controller/RedisTestController.java
@@ -1,12 +1,21 @@
 package com.cmh.item.biz.web.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.cmh.item.biz.sdk.service.redis.RedisService;
 import com.cmh.project.basis.base.ResultBuilder;
+import com.cmh.project.basis.utils.others.StringUtil;
+import com.google.common.base.Splitter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONObject;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 
 import javax.annotation.Resource;
+import javax.xml.transform.Result;
+import java.util.*;
 
 /**
  * @author：初明昊
@@ -21,6 +30,12 @@ public class RedisTestController {
     @Resource
     private RedisService redisService;
 
+    /**
+     * redis客户端请求s
+     */
+    @Resource
+    private Jedis jedisClient;
+
     @RequestMapping("/limit/count")
     public ResultBuilder<Boolean> limitCount(){
         ResultBuilder<Boolean> res = redisService.testLimit();
@@ -31,6 +46,26 @@ public class RedisTestController {
     public ResultBuilder<Boolean> limitRate(){
         ResultBuilder<Boolean> res = redisService.testRate();
         return res;
+    }
+
+    @RequestMapping("/set/key")
+    public ResultBuilder<Boolean> setNullKey(){
+        Set<String> str = new HashSet();
+        str.add("zhngsan");
+        str.add("zhngsan2");
+        str.add("zhngsan3");
+        jedisClient.set("test2",JSON.toJSONString(str));
+        return ResultBuilder.success();
+    }
+
+    @RequestMapping("/get/key")
+    public ResultBuilder<List> getNullKey(){
+        String str = jedisClient.get("test2");
+        List<String> listQuery = (List<String>) JSON.parse(str);
+        if(!CollectionUtils.isEmpty(listQuery)){
+            System.out.println("hello world");
+        }
+        return ResultBuilder.success(listQuery);
     }
 
     /*public boolean setnx(String key, String val) {
